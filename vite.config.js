708a44c9b8fcf5905f80673b903477ae89e83114
Simon Schrape <sim@ilitu.de>
2022-10-17 22:21:51 +0200

    Use Vite.

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    assetsInlineLimit: 1024,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        exampleLayout: resolve(__dirname, "example/layout.html"),
+        exampleBodyCopy: resolve(__dirname, "example/body-copy.html"),
+        exampleElementsShowcase: resolve(__dirname, "example/elements-showcase.html"),
+      },
+    },
+  },
+});
