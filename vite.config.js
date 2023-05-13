f3b7fad9fbd955e2e5a6f683279d7913e06726dd
Simon Schrape <sim@ilitu.de>
2023-05-13 23:40:23 +0200

    Use Handlebars to dedupe example HTML.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import handlebars from "vite-plugin-handlebars";
 
 export default defineConfig({
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, "partials"),
+    }),
+  ],
   build: {
     assetsInlineLimit: 0,
     rollupOptions: {
